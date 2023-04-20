27ae6ce422f56e22958390ac55cff1826ebdbf37
김동혁 <kdh1022255@gmail.com>
2023-04-20 21:40:43 +0900

    update

--- src/main/java/bser/kim/controller/ViewController.java
+++ src/main/java/bser/kim/controller/ViewController.java
@@ -1,13 +1,22 @@
 package bser.kim.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ViewController {
-    @GetMapping("/index")
+    @GetMapping("/")
     public String index(){
+        return "index";
+    }
+
+    @PostMapping("/")
+    public String index(String message, Model model){
+        System.out.println(message);
+        model.addAttribute("message", message);
         return "index";
     }
 }
